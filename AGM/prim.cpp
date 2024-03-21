95aaee5ff83b563a7459e6351d9fec836328983c
Aalasson <54062777+Aalasson@users.noreply.github.com>
2024-03-21 12:24:53 -0300

    Update prim.cpp

--- AGM/prim.cpp
+++ AGM/prim.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <stack>
 #include <algorithm>
 #include <limits> // for numeric_limits
-#include <queue> // for priority_queue
+#include <queue>  // for priority_queue
 
 using namespace std;
 
@@ -20,7 +19,42 @@ public:
         adj[to].push_back({from, weight}); // Para grafos não direcionados
     }
 
-    int prim(int startVertex = 0) {
+    vector<pair<int, int>> primEdges(int startVertex = 0) {
+        vector<bool> inMST(V, false);
+        vector<int> key(V, numeric_limits<int>::max());
+        vector<int> parent(V, -1);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        pq.push({0, startVertex});
+        key[startVertex] = 0;
+
+        while (!pq.empty()) {
+            int u = pq.top().second;
+            pq.pop();
+
+            inMST[u] = true;
+
+            for (auto neighbor : adj[u]) {
+                int v = neighbor.first;
+                int weight = neighbor.second;
+
+                if (!inMST[v] && key[v] > weight) {
+                    key[v] = weight;
+                    pq.push({key[v], v});
+                    parent[v] = u;
+                }
+            }
+        }
+
+        vector<pair<int, int>> edges;
+        for (int i = 1; i < V; ++i) {
+            edges.push_back({parent[i] + 1, i + 1}); // Adicionando 1 para ajustar índices para 1-indexado
+        }
+        return edges;
+    }
+
+    int primCost(int startVertex = 0) {
         vector<bool> inMST(V, false);
         vector<int> key(V, numeric_limits<int>::max());
         vector<int> parent(V, -1);
@@ -59,8 +93,6 @@ public:
 };
 
 int main(int argc, char *argv[]) {
-    
-
     string filename;
     bool showSolution = false;
     int startVertex = 0;
@@ -73,14 +105,12 @@ int main(int argc, char *argv[]) {
         } else if (string(argv[i]) == "-i" && i + 1 < argc) {
             startVertex = atoi(argv[i + 1]) - 1; // Corrigindo índice 1-indexado
         } else if (string(argv[i]) == "-h") {
-            cout << "Minimum Spanning Tree\n"
-                    "For the Minimum Spanning Tree problem, the Prim's algorithm requires the following parameters:\n"
-                    "-h : display this help message\n"
-                    "-o <file> : redirect output to 'file'\n"
-                    "-f <file> : specify the input graph 'file'\n"
-                    "-s : show the solution\n"
-                    "-i <vertex> : specify the start vertex (optional)\n"
-                    "$ ./prim -f input-file.dat -s -i 1" << endl;
+            cout << "-h           : mostra o help\n"
+                 << "-o <arquivo> : redireciona a saida para o <arquivo>'\n"
+                 << "-f <arquivo> : indica o <arquivo> que contem o grafo de entrada\n"
+                 << "-s           : mostra a solucao\n"
+                 << "-i           : vertice inicial (para o algoritmo de Prim)\n"
+                 << endl;
             return 0;
         }
     }
@@ -109,10 +139,17 @@ int main(int argc, char *argv[]) {
 
     file.close();
 
-    int mstCost = graph.prim(startVertex);
-
-    if (showSolution) {
-        cout << "Minimum Spanning Tree cost: " << mstCost << endl;
+    if (startVertex >= 0) {
+        if (!showSolution) {
+            int mstCost = graph.primCost(startVertex);
+            cout << mstCost << endl;
+        } else {
+            vector<pair<int, int>> edges = graph.primEdges(startVertex);
+            for (auto edge : edges) {
+                cout << "(" << edge.first << "," << edge.second << ") ";
+            }
+            cout << endl;
+        }
     }
 
     return 0;
